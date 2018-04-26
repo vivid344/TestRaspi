9610dc1d4da83b4c83d8b2feff736bf98c80e4ea
vivid344 <b1014127@fun.ac.jp>
2018-04-27 00:22:50 +0900

    [fix]GPIO10ピンをLチカするように変更

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 from time import sleep
+
+import random
 import pigpio
+
+# とりあえずGPIO18ピンをLチカ
 
 while True:
     pi = pigpio.pi()
-    pi.set_mode(18, pigpio.OUTPUT)
-    pi.write(18, 1)
-    sleep(0.05)
-    pi.write(18, 0)
-    sleep(0.05)
+    pi.set_mode(10, pigpio.OUTPUT)
+    pi.write(10, 1)
+    sleep(random.uniform(0,0.1))
+    pi.write(10, 0)
+    sleep(random.uniform(0,0.1))
